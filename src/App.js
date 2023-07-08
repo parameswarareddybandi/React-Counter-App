79946f04decde114f08ad65ae77ba4ca5ba896e9
parameswarareddybandi <parameswarareddybandi@gmail.com>
2023-07-09 01:33:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  let [counter, updateCounter] = useState(0);
+  let limit = 10;
+  let date = new Date();
+  var Day = date.getDay();
+  let arr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"]
+  var TodayDate = date.getDate();
+  var Month = date.getMonth()+1;
+  var Year = date.getFullYear();
+
+  return(
+    <div id='container'>
+      <h1>{arr[Day]}, {TodayDate}/{Month}/{Year}</h1>
+      <h2>Counter App Using React</h2>
+      <button id='minus' disabled={counter === 0} onClick={()=>{
+          if(counter > 0){
+            updateCounter(--counter);
+          }
+      }}> - </button>
+      <h3>{counter}</h3>
+      <button id='plus' disabled={counter === limit} onClick={()=>{
+          if(counter < limit){
+            updateCounter(++counter);
+          }
+      }}> + </button>
     </div>
-  );
+  )
 }
 
 export default App;
